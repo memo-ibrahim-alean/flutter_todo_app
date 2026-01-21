fe408ec101b651c3b712a96e0e64a1616ed6d9bf
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2026-01-21 22:08:41 +0200

    Polish TaskList styling with final adjustments

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -5,33 +5,37 @@ class TaskList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final tasks = [
-      'Buy groceries',
-      'Finish project',
-      'Call mom',
-      'Go to gym',
-      'Read book',
-    ];
+    final tasks = ['Buy groceries', 'Finish project', 'Call mom', 'Go to gym'];
 
     return Column(
       children: [
-        const Text(
-          'Tasks',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
         const SizedBox(height: 15),
         Expanded(
           child: ListView.builder(
             shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
             itemCount: tasks.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                leading: Checkbox(value: false, onChanged: null),
-                title: Text(tasks[index]),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete, color: Colors.red),
-                  onPressed: () {},
+              return Container(
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      // spreadRadius: 5,
+                      blurRadius: 3,
+                      // offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  leading: Checkbox(value: false, onChanged: null),
+                  title: Text(tasks[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {},
+                  ),
                 ),
               );
             },
